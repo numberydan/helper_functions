491fc70634bb8819b0c8502fcd532a9a6798cb37
numberydan <moultondan@gmail.com>
2020-03-09 20:51:13 -0400

    add in some nicer formating

--- python_helpers/flight_test.py
+++ python_helpers/flight_test.py
@@ -1,12 +1,7 @@
-import os
 import pandas as pd
-import numpy as np
-import copy
-from math import log
 import datetime
 from pandas.tseries.offsets import *
 import time
-import matplotlib
 import plotly
 from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
 init_notebook_mode()
@@ -18,55 +13,67 @@ from plotly import tools
 def to_unix_time(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     return (dt-epoch).total_seconds()*1000
-            
-def plotly_pandas_hack(data,title,x_axis_name,y_axis_name):
-    starting_dictionary = {'data': [],
-    'layout': {'title':title,
-    'xaxis':  {'title':x_axis_name,'range': [to_unix_time(data.index.min()), to_unix_time(data.index.max()+MonthBegin(2))]},
-    'yaxis':  {'title':y_axis_name},
-              }}
-    for columns in data.columns.tolist():
-        current = {
-            'mode': 'lines',
-            'name': columns,
-            'type': 'scatter',
-            'x': data.index,
-            'y': data[columns].values.tolist()
-        }
-        starting_dictionary["data"].append(current)
-    iplot(starting_dictionary)
 
-def plotly_pandas_hack_markers(data,title,x_axis_name,y_axis_name):
+# to do, bring back abilty to set y range
+# to do, bring back split axis ability
+# to do, bring back scaling
+# to do, make flexible for dates vs categorical
+
+dan_colorway = ["#2965CC", "#D13913", "#29A634", "#D99E0B", "#8F398F", "#00B3A4", "#DB2C6F", "#9BBF30", "#96622D", "#7157D9"]
+
+def plotly_pandas(data,title='Title'
+                       ,x_axis_name='X Axis',y_axis_name='Y Axis'
+                      ,show_max_date=True,mode='lines'):
     starting_dictionary = {'data': [],
-    'layout': {'title':title,
-    'xaxis':  {'title':x_axis_name,'range': [to_unix_time(data.index.min()), to_unix_time(data.index.max()+MonthBegin(2))]},
-    'yaxis':  {'title':y_axis_name},
-              }}
-    for columns in data.columns.tolist():
-        current = {
-            'mode': 'lines+markers',
-            'name': columns,
-            'type': 'scatter',
-            'x': data.index,
-            'y': data[columns].values.tolist()
-        }
-        starting_dictionary["data"].append(current)
-    iplot(starting_dictionary)    
+    'layout': {
+        'title':title
+        ,'legend':{'orientation':'h','xanchor':'center','y':1.1,'x':0.5}
+        ,'xaxis':  {'title':x_axis_name
+                    , 'showline':True
+                    , 'linewidth':1
+                    , 'linecolor':'#738694'
+                    , 'mirror':True
+                    , 'gridcolor':'#D8E1E8'
+                    ,'range': [to_unix_time(data.index.min())
+                              , to_unix_time(data.index.max()+MonthBegin(2))]
+                  }
+        ,'yaxis':  {'title':y_axis_name
+                    , 'showline':True
+                    , 'linewidth':1
+                    , 'linecolor':'#738694'
+                    , 'mirror':True
+                    , 'gridcolor':'#D8E1E8'
+                }
+        ,'paper_bgcolor':'#FFFFFF'
+        ,'plot_bgcolor':'#FFFFFF'
+        ,'colorway':dan_colorway
+                  }
+                          }
     
-def plotly_pandas_hack_markers_log(data,title,x_axis_name,y_axis_name):
-    starting_dictionary = {'data': [],
-    'layout': {'title':title,
-    'xaxis':  {'title':x_axis_name,'range': [to_unix_time(data.index.min()), to_unix_time(data.index.max()+MonthBegin(2))]},
-    'yaxis':  {'title':y_axis_name, 'type':'log'},
-              }}
+    
+    if show_max_date:
+        starting_dictionary['layout']['annotations'] = \
+            [
+                {
+                    'text':'Max Date: ' + str(data.index.max())[:10]
+                    ,'xref':'paper'
+                    ,'yref':'paper'
+                    ,'xanchor':'left'
+                    ,'x':-0.05
+                    ,'y':-0.15
+                    ,'showarrow':False
+                    ,'font':{'color':'#5C7080'}
+                }
+            ]
+    
     for columns in data.columns.tolist():
         current = {
-            'mode': 'lines+markers',
+            'mode': mode,
             'name': columns,
             'type': 'scatter',
             'x': data.index,
             'y': data[columns].values.tolist()
         }
         starting_dictionary["data"].append(current)
-    iplot(starting_dictionary)        
-
+        
+    iplot(starting_dictionary)
